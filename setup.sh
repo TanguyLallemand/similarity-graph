9f4a28d6b421eaef840797ec3baa2a911b58d118
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-10-09 19:09:35 +0200

    Add a default execution of script_python

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#conda create --name python_align --file python_environment_used.txt
+conda create --name python_align --file python_environment_used.txt
 source activate python_align
 chmod +x script_python.py
 ./script_python.py sequence.fasta -d
