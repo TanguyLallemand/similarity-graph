f73cc36e938086f129658cac8fdc62c2edcdd6f5
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-18 22:33:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda create --name python_align --file python_environment_used.txt
+conda create --name python_align --file ./Packages_used_for_virtual_env/python_environment_used.txt
 source activate python_align
 chmod +x script_python.py
-./script_python.py -f sequence.fasta -d
+./script_python.py -f ./fasta_files/sequence.fasta -d
