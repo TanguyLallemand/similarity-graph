b2a087bcc68e4e005eddeb67264b7d90846fe85b
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-19 14:24:17 +0200

    Rework project arborescence

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,5 +2,5 @@
 
 conda create --name python_align --file ./Packages_used_for_virtual_env/python_environment_used.txt
 source activate python_align
-chmod +x script_python.py
-./script_python.py -f ./fasta_files/sequence.fasta -d
+chmod +x ../python_align.py
+../python_align.py -f ../example_of_fasta_files/sequence.fasta -d
